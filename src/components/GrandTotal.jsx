41474e92bdec51a38efd6cab9628921e8abf09bc
Sahil jalandhara <sahiljalandhara123@gmail.com>
2024-06-20 08:49:03 +0530

    adding subtotal discount and tax field

--- src/components/GrandTotal.jsx
+++ src/components/GrandTotal.jsx
@@ -5,19 +5,63 @@ export function GrandTotal({ total }) {
     useEffect(() => {
         if (total.items.length == 0) {
         } else {
-           const to = total.items.reduce((acc, curr) => acc + curr.price, 0);
+            const to = total.items.reduce((acc, curr) => acc + curr.price, 0);
             if (!isNaN(to)) {
-                setTotal(to);
+                setTotal(to.toLocaleString("en-IN"));
             }
         }
-    }, [gtotal,total]);
+    }, [gtotal, total]);
     return (
         <>
-            <div className="w-full grid justify-end">
-                <div>Discount</div>
-                <div>Tax</div>
+            <div className="flex justify-around">
                 <div>
-                    Total <span>{gtotal}</span>
+                    <div>
+                        <label className="form-control">
+                            <div className="label">
+                                <span className="label-text">Notes</span>
+                            </div>
+                            <textarea
+                                className="textarea textarea-bordered h-24"
+                                placeholder="Any Notes"
+                            ></textarea>
+                        </label>
+                    </div>
+                    <div>
+                        <label className="form-control">
+                            <div className="label">
+                                <span className="label-text">Terms</span>
+                            </div>
+                            <textarea
+                                className="textarea textarea-bordered h-24"
+                                placeholder="Terms and condition: 14 days return"
+                            ></textarea>
+                        </label>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        SubTotal: <span>{`\u20B9${gtotal}`}</span>
+                    </div>
+                    <div className="flex">
+                        <input
+                            type="text"
+                            placeholder="Discount"
+                            className="input w-full max-w-xs"
+                        />
+                        <input
+                            type="number"
+                            placeholder="0"
+                            className="input input-bordered w-full max-w-xs"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            className="input input-bordered"
+                            placeholder="Daisy"
+                        />
+                        <label className="flex items-center">Name</label>
+                    </div>
                 </div>
             </div>
         </>
