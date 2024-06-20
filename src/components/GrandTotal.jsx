e37f7c1366d70f2e9eab352a56cd238c2a26e726
Sahil jalandhara <sahiljalandhara123@gmail.com>
2024-06-20 11:25:35 +0530

    adding state management to charges

--- src/components/GrandTotal.jsx
+++ src/components/GrandTotal.jsx
@@ -1,11 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import { Charge } from "./Charge";
-
+const CHARGE_ACTION = {
+    ADD_CHARGE: "add-charge",
+};
 export function GrandTotal({ total }) {
     const [gtotal, setTotal] = useState(0);
     const [charges, setCharges] = useState([{}]);
+    const [dis, setDis] = useState({ head: "Discount", rate: 0 });
+    const [state, dispatch] = useReducer(reducer, { surcharges: [] });
+
+    function reducer(state, action) {
+        switch (action.type) {
+            case CHARGE_ACTION.ADD_CHARGE:
+                return {
+                    surcharges: [
+                        ...state.surcharges.slice(0, action.payload.index),
+                        { head: "Tax", rate: 0 },
+                    ],
+                };
+            default:
+        }
+    }
     function addCharge() {
         setCharges([...charges, {}]);
+    }
+    function discountManage(e) {
+        const temp = { ...dis, rate: e.target.value };
+        setDis(temp);
+    }
+    function discountHead(e) {
+        const temp = { ...dis, head: e.target.value };
+        setDis(temp);
     }
     useEffect(() => {
         if (total.items.length == 0) {
@@ -47,7 +72,23 @@ export function GrandTotal({ total }) {
                     <div>
                         SubTotal: <span>{`\u20B9${gtotal}`}</span>
                     </div>
-                    <Charge placeholder="Discount"/>
+                    <div className="flex relative">
+                        <input
+                            type="text"
+                            placeholder="Discount"
+                            className="input w-full max-w-xs pl-10"
+                            onChange={discountHead}
+                            value={dis.head}
+                        />
+                        <button className="absolute top-1/4 left-2">%</button>
+                        <input
+                            type="number"
+                            placeholder="0"
+                            onChange={discountManage}
+                            value={dis.rate}
+                            className="input w-full max-w-xs pl-10"
+                        />
+                    </div>
                     {charges.map((item, index) => {
                         return <Charge placeholder="Tax" />;
                     })}
